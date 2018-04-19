bf7b86418571af43cba3279185c434856678b055
saif khan <sk2437@afsconnect1.njit.edu>
2018-04-19 02:56:13 -0400

    commit

---
+++ database.php
@@ -0,0 +1,42 @@
+<?php
+class Database {
+    private static $dsn = 'mysql:host=sql2.njit.edu;dbname=sk2437';
+    private static $username = 'sk2437';
+    private static $password = 'headroom5';
+    private static $db;
+    private function __construct() {}
+    public static function getDB () {
+        if (!isset(self::$db)) {
+            try {
+                self::$db = new PDO(self::$dsn,
+                                     self::$username,
+                                     self::$password);
+            } catch (PDOException $e) {
+                $error_message = $e->getMessage();
+                echo $error_message;
+                exit();
+            }
+        }
+        return self::$db;
+    }
+}
+
+class userobject {
+    private $firstField;
+    private $secondField;
+
+    public function __get($property) {
+      if (property_exists($this, $property)) {
+        return $this->$property;
+      }
+    }
+
+    public function __set($property, $value) {
+      if (property_exists($this, $property)) {
+        $this->$property = $value;
+      }
+
+      return $this;
+    }
+}
+?>
